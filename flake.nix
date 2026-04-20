ee4a0224f222ad54c953d87f1fc32a14d19fee0c
VictoryTek <victorytech@proton.me>
2026-04-20 09:07:13 -0500

    1.0.0 release

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "up";
-          version = "0.1.0";
+          version = "1.0.0";
           src = ./.;
 
           cargoLock = {
